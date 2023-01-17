115c5e3cc0234f81f5a0ff859fb67f2a284ba0be
Max <admin@example.com>
2023-01-17 12:14:38 +0200

    chore: remove layout

--- src/main/resources/files/src/App.tsx
+++ src/main/resources/files/src/App.tsx
@@ -28,42 +28,12 @@ function App() {
 	return (
 		
 			<Routes>
-        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="game" element={<Game />} />
           <Route path="*" element={<NotFound />} />
-        </Route>
 	</Routes>
       
   );
 }
 
 export default App;
-
-
-function Layout() {
-  return (
-    <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/game">Game</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
-      <Outlet />
-    </div>
-  );
-}
-
